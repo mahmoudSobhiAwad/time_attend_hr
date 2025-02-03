22e770a23a0528505128aa6224f0fc0c35611307
modyelgen <modyelgen243@gmail.com>
2025-02-03 09:11:05 +0200

    core setup

---
+++ android/app/src/main/kotlin/com/example/time_attend_hr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.time_attend_hr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/utils/theme/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String appLogo = 'assets/images/logo.svg';
+  static const String profileImage = 'assets/images/profile.svg';
+  static const String datePickerIcon = 'assets/images/date_picker_icon.svg';
+  static const String arrowIn = 'assets/images/arrow_in.svg';
+  static const String arrowOut = 'assets/images/arrow_out.svg';
+  static const String breakTimeIn = 'assets/images/break_time_in.svg';
+  static const String breakTimeOut = 'assets/images/break_time_out.svg';
+}
